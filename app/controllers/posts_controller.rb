2aeff691a69ce6dd7b6587a2779010ec82a1268c
helgakern <helgakern@gmail.com>
2020-04-29 16:49:31 -0700

    created post view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        post_params = param.require(:post).permit(:title, :body)
+        @post = Post.new post_params
+        if @post.save
+            render plain: 'Post Created'
+        else
+            render :new
+        end
+    end
 end

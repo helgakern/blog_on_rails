679c9c31046c314fc0c83e0b737024d0392120a8
helgakern <helgakern@gmail.com>
2020-04-29 16:20:58 -0700

    added validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+
+    validates :title, {
+        presence: true,
+        uniqueness: { case_sensitive: true}
+    }
+
+    validates :body, {
+        presence: true,
+        length: { minimum: 50}
+    }
 end

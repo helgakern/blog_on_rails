006d83b392328f4038247c4301e1dfc3724173ce
helgakern <helgakern@gmail.com>
2020-04-29 20:55:58 -0700

    migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,58 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Post.destroy_all
+Comment.delete_all
+Post.delete_all
+User.delete_all
 
-50.times do
-    Post.create({
-        title: Faker::Hacker.noun,
-        body: Faker::Hacker.say_something_smart
-    })
+PASSWORD = "supersecret" 
+
+super_user = User.create(
+    name: "helga",
+    email: "helga@gmail.com",
+    password: PASSWORD,
+    is_admin: true
+)
+
+10.times do
+    name = Faker::Name.middle_name
+    User.create(
+        name: name,
+        email: "#{name.downcase}@gmail.com",
+        password: PASSWORD
+    )
 end
 
-puts "Created #{Post.count} posts"
+users = User.all
+puts "created #{users.count} users"
+
+50.times do
+    user= users.sample
+    p = Post.create(
+        title: Faker::Hacker.say_something_smart,
+        body: Faker::ChuckNorris.fact,
+        created_at: Faker::Date.backward(days:365 * 5),
+        updated_at: Faker::Date.backward(days:365 * 5),
+        user_id: user.id
+    )
+    if p.valid?
+        p.comments = rand(0..15).times.map do
+            user= users.sample
+            Comment.new({
+                message: Faker::GreekPhilosophers.quote, 
+                user_id: user.id
+            })
+        end
+    else
+        puts "Invalid post!"
+        puts p
+    end
+end
+
+
+    posts = Post.all
+    puts "created #{posts.count} posts"
+    comments = Comment.all
+    puts "created #{comments.count} comments"
+
+

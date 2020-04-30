3efea98fbb397e658ca9ad078bf2270fb4fc5e8c
helgakern <helgakern@gmail.com>
2020-04-29 18:05:21 -0700

    updated controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,30 +1,63 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :load_post!, except: [:create]
+    before_action :authorize_user!, only: [:edit, :update, :destroy]
 
     def new
-        @post = Post.new
     end
 
     def create
-        post_params = param.require(:post).permit(:title, :body)
         @post = Post.new post_params
+        @post.user = @current_user
         if @post.save
-            redirect_to @post
+            render(plain: "Created Post #{@post.inspect}")
         else
             render :new
         end
     end
 
     def show
-        @post = Post.find params[:id]
+        @comments = Comment.new
     end
 
     def index
-        @posts = Post.order(created_at: :DESC)
+        @posts = Post.all.order('created_at DESC')
     end
 
     def destroy
-        @post = Post.find(params[:id])
         @post.destroy
         redirect_to posts_path
+    end
+
+    def edit
+    end
+
+    def update
+        if @post.update post_params
+            redirect_to posts_path @post
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
+
+    def authorize_user!
+        unless can? :crud, @post
+            flash[:danger] = "Access Denied"
+            redirect_to root_path
+        end
+    end
+
+    def load_post!
+        if params[:id].present?
+            @post = Post.find(params[:id])
+        else
+            @post = Post.new
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
 
-  root 'posts#index'
-  get "/posts/new" => "posts#new", as: :new_post
-  post "/posts" => "posts#create", as: :posts
-  get "/posts/:id" => "posts#show", as: :post
-  get "/posts" => "posts#index"
-  delete "/posts/:id" => "posts#destroy"
+  # root 'posts#index'
+  # get "/posts/new" => "posts#new", as: :new_post
+  # post "/posts" => "posts#create", as: :posts
+  # get "/posts/:id" => "posts#show", as: :post
+  # get "/posts" => "posts#index"
+  # delete "/posts/:id" => "posts#destroy"
 end

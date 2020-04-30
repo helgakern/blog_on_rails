def8c7db3cddc17eb00bd76cbffb0412b72a964f
helgakern <helgakern@gmail.com>
2020-04-29 17:08:32 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources :posts do
-  resources :comments, only: [:create, :destroy]
-end
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+  end
 
-
+  root 'posts#index'
+  get "/posts/new" => "posts#new", as: :new_post
+  post "/posts" => "posts#create", as: :posts
+  get "/posts/:id" => "posts#show", as: :post
+  get "/posts" => "posts#index"
 end

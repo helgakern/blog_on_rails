1e11763a11dd3c77539c720cb3b2fcf558a070cb
helgakern <helgakern@gmail.com>
2020-04-29 20:48:55 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,31 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :posts do
-    resources :comments, only: [:create, :destroy]
-  end
+  root 'posts#index'
 
-  # root 'posts#index'
-  # get "/posts/new" => "posts#new", as: :new_post
-  # post "/posts" => "posts#create", as: :posts
-  # get "/posts/:id" => "posts#show", as: :post
-  # get "/posts" => "posts#index"
-  # delete "/posts/:id" => "posts#destroy"
+  # index
+  get('/posts', to: 'posts#index')
+  # new
+  get('/posts/new', to: 'posts#new')
+  post('/posts', to:'posts#create', as:'create')
+  # show
+  get('/posts/:id', to:'posts#show', as:'post')
+  # edit
+  get('/posts/:id/edit',to:'posts#edit', as:'edit_post')
+  patch('/posts/:id/', to:'posts#update')
+  #delete
+  delete('/posts/:id', to:'posts#destroy')
+
+  #comments
+    # new
+    post('/posts/:post_id/comments/create', to:'comments#create', as:'post_comments')
+    #delete
+    delete('/posts/:post_id/comments/:id', to:'comments#destroy', as:'post_comment')
+
+    resources :users, only: [:new, :create, :update, :edit]
+    get('/user/:id/edit_password', to:'users#edit_password', as:'edit_user_password')
+    patch('/user/:id/edit_password', to:'users#update_password', as:'update_user_password')
+
+    resource :sessions, only: [:create, :destroy]
+    get('/session/new', to:'sessions#new', as:'new_session')
+
 end
